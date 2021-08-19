b65e15508d6e50e6e8d6683e975831f88d70e35c
Feng Shao <shaofeng@jibudata.com>
2021-08-19 13:32:43 +0800

    fix: add crds.yaml to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 kubectl apply -f deploy/cluster-roles.yaml
+kubectl apply -f deploy/crds.yaml
 kubectl apply -f deploy/deployment.yaml
 kubectl apply -f deploy/tenant_operator_deployment.yaml
