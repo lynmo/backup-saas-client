373aa078ca49f6097429c0b104613f30747d9f6c
Feng Shao <shaofeng@jibudata.com>
2021-09-06 10:23:21 +0800

    fix: update tenant operator yaml

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 kubectl apply -f deploy/cluster-roles.yaml
 kubectl apply -f deploy/crds.yaml
 kubectl apply -f deploy/deployment.yaml
-kubectl apply -f deploy/tenant_operator_deployment.yaml
+kubectl apply -f deploy/tenant-operator-deployment.yaml
