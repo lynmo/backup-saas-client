3daea9901b3d69d70afe8a1a3e5fc0095fe9c4e0
Feng Shao <shaofeng@jibudata.com>
2021-09-07 13:28:11 +0800

    feat: add yaml for qiming-operator roles

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 kubectl apply -f deploy/crds.yaml
+kubectl apply -f deploy/backend-controller-rbac.yaml
 kubectl apply -f deploy/deployment.yaml
 kubectl apply -f deploy/tenant-operator-deployment.yaml
