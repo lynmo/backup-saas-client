e331049c415ee670ecc64ce243495e79b7b8a9af
Feng Shao <shaofeng@jibudata.com>
2021-09-13 18:49:15 +0800

    fix: undeploy.sh --include-clusters

--- undeploy.sh
+++ undeploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [[ "$1" == "--include-clusters" ]];then
+    DELETE_MANAGED_CLUSTERS=1
+fi
+
 HOST_NS=backup-saas-system
 TENANT_NS_PREFIX=backup-tenant-
 
@@ -14,30 +18,40 @@ kubectl -n ${HOST_NS} get tenants.ys.jibudata.com -o go-template='{{range $ns :=
     kubectl -n ${TENANT_NS} delete --ignore-not-found=true deployment mig-controller-${tenant}
     kubectl -n ${TENANT_NS} delete --ignore-not-found=true deployment ui-discovery-${tenant}
 
-    # TODO: delete ns, clusterrolebindings from managed clusters
-    # # load kubeconfig of tenant, then issue below commands
-    # kubectl delete --ignore-not-found=true namespace qiming-backend
-    # kubectl delete --ignore-not-found=true ClusterRoleBinding qiming-backup-admin-rb
-    # for clusterrole in "qiming-operator-proxy-role" "qiming-operator-role"; do
-    #     kubectl get ClusterRoleBinding --all-namespaces -o go-template='{{range $cr := .items}}{{if eq $cr.roleRef.name "'${clusterrole}'"}}{{$cr.metadata.name}}{{"\n"}}{{end}}{{end}}' | while read -r rolebinding;do
-    #         kubectl delete --ignore-not-found=true ClusterRoleBinding ${rolebinding}
-    #     done
-    # done
+    if [[ "${DELETE_MANAGED_CLUSTERS}" == "1" ]];then
+        kubectl -n ${TENANT_NS} get clusters.ys.jibudata.com -o go-template='{{range $cluster := .items}}{{$cluster.metadata.name}}{{printf "\n"}}{{end}}' | while read -r cluster;do
+            echo "delete resources from managed cluster ${cluster}"
+            kubeconfig=$(mktemp /tmp/backup-saas-cluster-kubeconfig.XXXXXX)
+            kubectl -n ${TENANT_NS} get clusters.ys.jibudata.com ${cluster} -o go-template='{{.spec.kubeconfig}}' > ${kubeconfig}
+
+            kubectl --kubeconfig ${kubeconfig} delete --ignore-not-found=true namespace qiming-backend
+            kubectl --kubeconfig ${kubeconfig} delete --ignore-not-found=true ClusterRoleBinding qiming-backup-admin-rb
+            for clusterrole in "qiming-operator-proxy-role" "qiming-operator-role"; do
+                kubectl --kubeconfig ${kubeconfig} get ClusterRoleBinding --all-namespaces -o go-template='{{range $cr := .items}}{{if eq $cr.roleRef.name "'${clusterrole}'"}}{{$cr.metadata.name}}{{"\n"}}{{end}}{{end}}' | while read -r rolebinding;do
+                    kubectl --kubeconfig ${kubeconfig} delete --ignore-not-found=true ClusterRoleBinding ${rolebinding}
+                done
+            done
+
+            rm ${kubeconfig}
+        done
+    fi
 
     # remove finalizers
     cat <(kubectl api-resources --namespaced=true --api-group=ys.jibudata.com -o name) <(kubectl api-resources --namespaced=true --api-group=migration.yinhestor.com -o name) | while read -r res; do
         echo "clearing finalizer from all ${res} in ${TENANT_NS}"
-        # kubectl -n ${TENANT_NS} patch ${res} --all -p '{"metadata":{"finalizers":[]}}' --type=merge
         kubectl -n ${TENANT_NS} get ${res} -o go-template='{{range $ns := .items}}{{$ns.metadata.name}}{{printf "\n"}}{{end}}' | while read -r cr;do
-        echo "clearing finalizer (if exists) from ${res}: ${cr} in ${TENANT_NS}"
+            echo "deleting ${res} ${cr} from ${TENANT_NS}"
+            kubectl -n ${TENANT_NS} delete --ignore-not-found ${res} ${cr} --wait=false
+            echo "clearing finalizer (if exists) from ${res}: ${cr} in ${TENANT_NS}"
             kubectl -n ${TENANT_NS} patch ${res} ${cr} -p '{"metadata":{"finalizers":[]}}' --type=merge
         done
     done
+    echo "deleting tenant namespace ${TENANT_NS}"
+    kubectl delete --ignore-not-found=true namespace ${TENANT_NS} --wait=false
     echo "clearing finalizer of namespace ${TENANT_NS}"
     kubectl patch namespace ${TENANT_NS} -R -p '{"metadata":{"finalizers":[]}}' --type=merge
-    echo "deleting tenant namespace ${TENANT_NS}"
-    kubectl delete --ignore-not-found=true namespace ${TENANT_NS}
-    # kubectl -n ${HOST_NS} delete --ignore-not-found=true tenants.ys.jibudata.com ${tenant}
+    echo "deleting tenant ${tenant}"
+    kubectl -n ${HOST_NS} delete --ignore-not-found=true tenants.ys.jibudata.com ${tenant} --wait=false
     echo "clearing finalizer of tenant ${tenant} in ${HOST_NS}"
     kubectl -n ${HOST_NS} patch tenants.ys.jibudata.com ${tenant} -p '{"metadata":{"finalizers":[]}}' --type=merge
 done
@@ -55,11 +69,13 @@ echo "deleting tenant operator deployment"
 kubectl delete --ignore-not-found=true -f deploy/tenant-operator-deployment.yaml
 
 echo "done"
-echo "-------------------------------------------------------------------------------------------------------"
-echo "|please delete below resources from all managed clusters:                                             |"
-echo "|Namespace: qiming-backend                                                                            |"
-echo "|ClusterRoleBinding: qiming-backup-admin-rb, velero-installer-rolebinding                             |"
-echo "|all ClusterRoleBinding that bound to ClusterRole qiming-operator-role and qiming-operator-proxy-role |"
-echo "-------------------------------------------------------------------------------------------------------"
+if [[ "${DELETE_MANAGED_CLUSTERS}" != "1" ]];then
+    echo "-------------------------------------------------------------------------------------------------------"
+    echo "|please delete below resources from all managed clusters:                                             |"
+    echo "|Namespace: qiming-backend                                                                            |"
+    echo "|ClusterRoleBinding: qiming-backup-admin-rb, velero-installer-rolebinding                             |"
+    echo "|all ClusterRoleBinding that bound to ClusterRole qiming-operator-role and qiming-operator-proxy-role |"
+    echo "-------------------------------------------------------------------------------------------------------"
+fi
 
 exit 0
